ff4dcf9206e7e26cf5bdaa44a75a291f56af417e
CorneliusBroderick <9119124@studentmail.ul.ie>
2018-10-30 13:33:05 +0000

    Just starting Food Rater project.

---
+++ Foodrater/app/src/main/java/ie/ul/cbroderick/foodrater/MainActivity.java
@@ -0,0 +1,47 @@
+package ie.ul.cbroderick.foodrater;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        //done: connect recylcer view to our  food Adapter
+        final FoodAdapter foodAdapter = new FoodAdapter();
+        recyclerView.setAdapter(foodAdapter);
+
+
+        foodAdapter.addFood();
+        foodAdapter.addFood();
+        foodAdapter.addFood();
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                foodAdapter.addFood();
+            }
+        });
+    }
+
+}
